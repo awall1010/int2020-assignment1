a5c62f6e344fe70adc91471a11543a8daf7ce471
root <awall@chapman.edu>
2020-01-13 20:39:35 +0000

    this is my 1st  attempted commit

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+  cout<<"Hello World CPSC 298";
+  cout<< endl;
+  cout<<"What is happening" << endl;
+
+
+
+  return 0;
+}
